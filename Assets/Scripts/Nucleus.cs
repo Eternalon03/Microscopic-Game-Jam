5840c1a5e9cc0cb9414a685e37968c84bd0c6f6e
Nicole Planeta <60528262+Eternalon03@users.noreply.github.com>
2023-06-11 15:36:07 -0400

    fixed nucleus

--- Assets/Scripts/Nucleus.cs
+++ Assets/Scripts/Nucleus.cs
@@ -6,8 +6,6 @@ using TMPro;
 public class Nucleus : MonoBehaviour
 {
     GameManager gm;
-
-    public GameObject player;
     float speed;
     float startTime;
     public TextMeshProUGUI label;
@@ -21,8 +19,6 @@ public class Nucleus : MonoBehaviour
     {
         // This game is a win when the timer runs out
         gm.loseOnTimer = false;
-
-        player.transform.position = new Vector3(5, -1.01f, 0);
 
         // Randomly choose whether the nucleus starts on the top or the bottom
         int startPosition = Random.Range(0, 2);
